6d764c9dbc2852e4fe574d3625e6043d7838958a
houfujian <houfujian@jd.com>
2021-05-06 09:54:23 +0800

    feat: 插件调试

--- plugins/MPWebpackPlugin.js
+++ plugins/MPWebpackPlugin.js
@@ -1,12 +1,19 @@
 const EntryPlugin = require('webpack/lib/EntryPlugin')
+const path = require('path')
 
 class MPWebpackPlugin {
     constructor() {
 
     }
 
-    apply() {
-        // new EntryPlugin()
+    apply(compiler) {
+        const { options, context } = compiler;
+        const secondEntry = path.resolve(context, './src/pages/address/address.js')
+        compiler.hooks.make.tap('run', (compilation) => {
+            const dep = EntryPlugin.createDependency(secondEntry, options);
+            compilation.addEntry(context, dep, options, err => {});
+        })
+        console.log(compiler);
     }
 }
 
